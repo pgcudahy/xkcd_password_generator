9e317b2abec1c602e17fb378218b208359c40fbc
Patrick Cudahy <pgcudahy@gmail.com>
2025-01-04 19:37:16 -0500

    add download of source word file

--- word_frequency.R
+++ word_frequency.R
@@ -1,12 +1,15 @@
 library(tidyverse)
 library(readxl)
+
+download.file("https://www.wordfrequency.info/samples/wordFrequency.xlsx",
+    "wordFrequency.xlsx")
+
 word_frequency <- read_excel("wordFrequency.xlsx", sheet = "1 lemmas")
+
 words <- word_frequency %>%
     filter(nchar(lemma) >= 3) %>%
     filter(!str_detect(lemma, "[^a-zA-Z]")) %>%
-    mutate(lemma2 = str_to_lower(lemma)) %>%
-    pull(lemma2)
-words2 <- c("Obtained from www.wordfrequency.info",
-    words) %>% tibble()
-write_csv(words2, "words.csv",
-    col_names = FALSE)
+    mutate("Obtained_from_www.wordfrequency.info" = str_to_lower(lemma)) %>%
+    select("Obtained_from_www.wordfrequency.info")
+
+write_csv(words, "words.csv")
